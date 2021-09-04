133f8e62eca5743ed2ea2a54cae066b3ffd01770
Loïc Demay <fruityenloops@gmail.com>
2021-09-04 20:08:38 +0200

    Format date and check for date

--- commands/edt.js
+++ commands/edt.js
@@ -1,4 +1,6 @@
 const ical = require('node-ical');
+const calendars = require('../calendars.json');
+const Discord = require('discord.js');
 
 module.exports = {
 	name: 'edt',
@@ -8,16 +10,35 @@ module.exports = {
         // 
         // TODO : Search for calendar url based on roles 
         // let url = getCalUrlFromRoles(message.member.roles);
-		printIcal('https://calendar.google.com/calendar/ical/hdpka717lurrk1qu3pds5q7u40@group.calendar.google.com/public/basic.ics', new Date()); // sample ical
+		printIcal(message, calendars[0], new Date("2021-09-03"));
 	},
 };
 
-function printIcal(url, date){
-    ical.async.fromURL(url, {}, function (err, data) {
+function printIcal(message, calendar, date){
+    const response = new Discord.MessageEmbed()
+        .setTitle(date.toDateString() + " - " + calendar.alias)
+        .setAuthor("HoursAndCounting");
+    ical.async.fromURL(calendar.url, {}, function (err, data) {
+        console.log(data);
         for(let element in data){
             if(data[element].type == 'VEVENT'){
-                // check for date; display if necessary
+                if(sameDay(new Date(data[element].start), date)) {
+                    let startTime = new Date(data[element].start);
+                    let endTime = new Date(data[element].end);
+                    response.addField(
+                        data[element].description.val,
+                        startTime.getHours() + "h" + startTime.getMinutes() + " -> " + endTime.getHours() + "h" + endTime.getMinutes()
+                    );
+                }
             }
         }
-    });
+        message.channel.send(response);
+    });    
 }
+
+// https://stackoverflow.com/a/43855221
+function sameDay(d1, d2) {
+    return d1.getFullYear() === d2.getFullYear() &&
+      d1.getMonth() === d2.getMonth() &&
+      d1.getDate() === d2.getDate();
+  }
