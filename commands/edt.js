fe1504acf5b8e13e9e84fae562998b10d47441e1
Loïc Demay <fruityenloops@gmail.com>
2021-09-05 13:02:57 +0200

    Role, date and calendar alias parsing

--- commands/edt.js
+++ commands/edt.js
@@ -4,18 +4,58 @@ const Discord = require('discord.js');
 
 module.exports = {
 	name: 'edt',
-	description: 'Get the current day\'s timetable for your or a specified group.',
-	execute(message) {
-        // TODO : read args for date / specific calendar
-        // let date = processArgsForDate(message);
-        // otherwise use today
-        // let date = new Date();
-        //
-        // TODO : Search for calendar based on message if applicable
-        // let calendar = processArgsForCalendar(message);
-        // TODO : Search for calendar based on roles 
-        // let calendar = getCalUrlFromRoles(message.member.roles);
-		printIcal(message, calendars[0], new Date("2021-09-03"));
+	description: 'Get the specified day\'s timetable for your or a specified group.',
+	execute(message, args) {
+        let date;
+        let cal;
+        
+        for(let arg of args){
+            arg = arg.toLowerCase();
+
+            // TODO : clean this messy switch
+            switch (arg) {
+                case 'demain':
+                    date = new Date();
+                    date.setDate(date.getDate() + 1);
+                    break;
+                case 'lundi':
+                    date = getNextDayOfWeek(new Date(), 1);
+                    break;
+                case 'mardi':
+                    date = getNextDayOfWeek(new Date(), 2);
+                    break;
+                case 'mercredi':
+                    date = getNextDayOfWeek(new Date(), 3);
+                    break;
+                case 'jeudi':
+                    date = getNextDayOfWeek(new Date(), 4);
+                    break;
+                case 'vendredi':
+                    date = getNextDayOfWeek(new Date(), 5);
+                    break;
+                case 'samedi':
+                    date = getNextDayOfWeek(new Date(), 6);
+                    break;
+                case 'dimanche':
+                    date = getNextDayOfWeek(new Date(), 7);
+                    break;
+            }
+            
+            calendar = searchForCalendarInArg(arg);
+        }
+
+        if(date == null) {
+            date = new Date(); // if date has not been defined by user, use today as default
+        }
+
+        if(cal == null) {
+            cal = getCalendarFromRoles(message.member); // if no calendar was specified during args parsing, try to find one in the user's roles
+            if(cal == null) {
+                message.channel.send('error : no suitable arguments or role found');
+                return;
+            }
+        }
+		printIcal(message, cal, date);
 	},
 };
 
@@ -37,7 +77,27 @@ function printIcal(message, calendar, date){
             }
         }
         message.channel.send(response);
-    });    
+    });
+}
+
+function searchForCalendarInArg(arg) {
+    for(let calendar of calendars) {
+        for(let alias of calendar.alias) {
+            if(alias.toLowerCase() == arg) {
+                return calendar;
+            }
+        }
+    }
+}
+
+function getCalendarFromRoles(member) {
+    for(let calendar of calendars) {
+        for(let alias of calendar.alias) {
+            if(member.roles.cache.some(role => role.name.toLowerCase() === alias.toLowerCase())) {
+                return calendar;
+            }
+        }
+    }
 }
 
 // https://stackoverflow.com/a/43855221
@@ -45,4 +105,10 @@ function sameDay(d1, d2) {
     return d1.getFullYear() === d2.getFullYear() &&
       d1.getMonth() === d2.getMonth() &&
       d1.getDate() === d2.getDate();
-  }
+}
+
+function getNextDayOfWeek(date, dayOfWeek) {
+    var resultDate = new Date(date.getTime());
+    resultDate.setDate(date.getDate() + (7 + dayOfWeek - date.getDay()) % 7);
+    return resultDate;
+}
