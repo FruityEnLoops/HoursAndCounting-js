cb0b17d698e34224380205f79c8ea87999922655
Loïc Demay <fruityenloops@gmail.com>
2021-09-05 17:03:01 +0200

    Help command

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,22 @@
+const Discord = require('discord.js');
+const { goodColor } = require('../config.json');
+
 module.exports = {
 	name: 'help',
 	description: 'Display the help message.',
 	execute(message) {
-		// TODO
-        // message.channel.send("");
+		const response = new Discord.MessageEmbed()
+			.setTitle('Aide')
+			.setURL('https://github.com/FruityEnLoops/HoursAndCounting-js')
+			.setAuthor('HoursAndCounting', 'https://i.imgur.com/J27Atoa.png', 'https://github.com/FruityEnLoops/HoursAndCounting-js')
+			.setColor(goodColor)
+			.setFooter('Développé par blobdash', 'https://i.imgur.com/7dIyraU.png')
+		
+		response.addFields(
+			{name: '!edt [demain || jour] [groupe]', value: `Affiche le calendrier d'aujourd'hui / du jour spécifié, pour le groupe dont vous avez le rôle / du calendrier spécifié.`},
+			{name: '!edtlist', value: 'Affiche la liste des calendriers, et les alias par lesquels ils peuvent être appelés.'},
+			{name: '!help', value: `Affiche ce message d'aide.`}
+		);
+		message.channel.send(response);
 	},
 };
